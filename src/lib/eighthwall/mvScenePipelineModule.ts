4f1d868915677c2259772829b47fa4cb9f3b6ff6
atsuhan <atsuhan220@gmail.com>
2020-05-10 17:50:26 +0900

    add decoration

--- src/lib/eighthwall/mvScenePipelineModule.ts
+++ src/lib/eighthwall/mvScenePipelineModule.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import ThreeAmbientLight from '../three/objects/ThreeAmbientLight';
 import ThreeGround from 'src/lib/three/objects/ThreeGround';
 import ThreeRaycaster from '../three/utils/ThreeRaycaster';
@@ -6,6 +7,15 @@ import ThreeVideoPlane from 'src/lib/three/objects/ThreeVideoPlane';
 import XrThreeBase from 'src/lib/eighthwall/XrThreeBase';
 
 const VIDEO_ELM_CLASSNAME = `.video-hoshino`;
+const DECORATION_PATHES = [
+  `/img/webar/mv/decorations/DOT.png`,
+  `/img/webar/mv/decorations/FRAME_SMALL.png`,
+  `/img/webar/mv/decorations/FRAME.png`,
+  `/img/webar/mv/decorations/RING.png`,
+  `/img/webar/mv/decorations/SQUARE_SMALL.png`,
+  `/img/webar/mv/decorations/TRIANGLE_FILL.png`,
+  `/img/webar/mv/decorations/TRIANGLE_FRAME.png`,
+];
 
 let threeAmbientLight;
 let threeBase: XrThreeBase;
@@ -13,8 +23,14 @@ let threeGround: ThreeGround;
 let threeVideoPlane: ThreeVideoPlane;
 let threeLyricTex: ThreeUnlitTexture;
 
+const threeDecorations: Array<ThreeUnlitTexture> = [];
+
 const onStart = async () => {
   threeBase = new XrThreeBase();
+
+  // Ambient Light
+  threeAmbientLight = new ThreeAmbientLight();
+  threeAmbientLight.addTo(threeBase.scene);
 
   // Ground
   threeGround = new ThreeGround();
@@ -29,31 +45,53 @@ const onStart = async () => {
 
   // lyric
   threeLyricTex = new ThreeUnlitTexture({
-    texPath: `/img/webar/mv/01_FILL.png`,
+    texPath: `/img/webar/mv/lyrics/01_FILL.png`,
   });
   await threeLyricTex.init();
   threeLyricTex.obj.scale.set(1.2, 0.1, 1);
   //threeLyricTex.setScaleAspect(1.2);
 
+  // decorations
+  await Promise.all(
+    _.map(DECORATION_PATHES, async (path) => {
+      const texPlane = new ThreeUnlitTexture({
+        texPath: path,
+      });
+      await texPlane.init();
+      texPlane.obj.scale.set(0.5, 0.5, 1);
+      threeDecorations.push(texPlane);
+    })
+  );
+
   // touchevent
   window.addEventListener(`touchstart`, (e) => {
     const raycaster = new ThreeRaycaster();
     const rayPos = raycaster.getRayIntersectPos(e, threeBase.camera, [threeGround.obj]);
-    threeVideoPlane.move(rayPos);
-    threeVideoPlane.lookAtAxisY(threeBase.camera);
 
+    // first tap
     if (!threeVideoPlane.parent) {
+      // video
       threeVideoPlane.play();
       threeVideoPlane.addTo(threeBase.scene);
 
+      // lyric
       threeLyricTex.addTo(threeVideoPlane.obj);
       threeLyricTex.move(new THREE.Vector3(0, -0.5, 0.5));
-    }
-  });
 
-  // Ambient Light
-  threeAmbientLight = new ThreeAmbientLight();
-  threeAmbientLight.addTo(threeBase.scene);
+      // decoration
+      _.forEach(threeDecorations, (deco) => {
+        deco.addTo(threeVideoPlane.obj);
+        deco.move(new THREE.Vector3(_.random(-1, 1, true), _.random(0.2, 1, true), _.random(-1, 1, true)));
+      });
+    }
+
+    // every tap
+    threeVideoPlane.move(rayPos);
+    threeVideoPlane.lookAtAxisY(threeBase.camera);
+    _.forEach(threeDecorations, (deco) => {
+      deco.move(new THREE.Vector3(_.random(-1, 1, true), _.random(0.2, 1, true), _.random(-1, 1, true)));
+    });
+  });
 };
 
 const onUpdate = () => {
